d357e426aef71940701374a808c2f0f2e74832d4
coffe <coffe@debian.azharazhar.com>
2020-09-15 11:01:04 +0800

    added lr scikit-learn

--- src/app.py
+++ src/app.py
@@ -7,64 +7,39 @@ Copyright 2020 CHASM.ML
 Basic Linear Regression in Python
 Using Scikit learn
 '''
-
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
+# Importing all dependencies
 import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
-# Define root directory for development purpose
+from sklearn.model_selection import train_test_split 
+from sklearn.linear_model import LinearRegression
+
+# Define root directory for accessing dataset
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 DATASET_PATH = f'{ROOT_DIR}/assets/dataset/student_scores.csv'
 
-dataset = pd.read_csv(DATASET_PATH)
+df = pd.read_csv(DATASET_PATH)
 
-# Create train data from the dataset
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 1].values
+df.fillna(method='ffill', inplace=True)
 
-# Importing scickit-learn
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
+# Define the data to feed the model
+X = np.array(df['Hours']).reshape(-1, 1)
+y = np.array(df['Scores']).reshape(-1, 1)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+df.dropna(inplace=True)
 
-# Train the model 
-regressor = LinearRegression()
-regressor.fit(X_train, y_train)
+# Splitting the data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-accuracy = regressor.coef_
-loss = regressor.intercept_
+# Define the model and train the model 
+regr = LinearRegression()
+regr.fit(X_train, y_train)
 
-# Printing the accuracy and the loss function
-print(f'accuracy: {accuracy[0]}@loss: {loss}')
+y_pred = regr.predict(X_test)
 
-# Predict the value
-y_pred = regressor.predict(X_test)
-
-line = loss + accuracy * X_test
-
-'''
-for v in X:
-    line.append(v + loss)
-
-print(line)
-'''
-
-# Creating GUI to make readable
-df = pd.DataFrame({'A': y_test, 'B': y_pred})
-df.plot(x='A', y='B', style='o')
-plt.plot(y_pred)
-plt.title('Index')
-plt.xlabel('Hours studied')
-plt.ylabel('Percentage scores')
+# Plotting the results
+plt.scatter(X_test, y_test, color='b')
+plt.plot(X_test, y_pred, color='k')
 plt.show()
-
-'''
-dataset.plot(x='Hours', y='Scores', style='o')
-plt.plot(y_pred)
-plt.title('Index')
-plt.xlabel('Hours studied')
-plt.ylabel('Percentage scores')
-plt.show()
-'''
